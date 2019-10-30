6372bda38fc6fb11e3f24ca9ca3dc705ea099293
Will Blew <will@willblew.com>
2019-10-30 00:01:49 +0000

    It's alive

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = "Make it.";
+require('header.php');
+?>
+
+<div class="splash-container">
+    <div class="splash">
+        <h1 class="splash-head">Blang Labs</h1>
+        <p class="splash-subhead">
+            We make cool stuff and share it with the world.
+        </p>
+    </div>
+</div>
+</body>
+</html>
---
+++ products.php
@@ -0,0 +1,20 @@
+<?php
+$title = "Products we've created.";
+require('header.php');
+?>
+<div class="splash-container">
+    <div class="splash">
+        <h1 class="splash-head">Blang Labs</h1>
+        <p class="splash-subhead">
+        	Products
+		</p>
+		<div class="pure-g">
+			<div class="pure-u-1-3"><p><a href="https://krl.io" alt="Krlio link" target="_new" style="color:white">Krlio</a></p></div>
+    		<div class="pure-u-1-3"><p>IN DEV</p></div>
+    		<div class="pure-u-1-3"><p>IN DEV</p></div>
+		</div>
+    </div>
+</div>
+
+</body>
+</html>
